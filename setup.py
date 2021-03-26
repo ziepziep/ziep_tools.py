4fee93cb1a510655ec5a5777da4c720827223448
ziepziep <hugo.eidmann@hotmail.com>
2021-03-26 14:13:05 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ziep_tools",
+    packages=find_packages(),
+    version="0.1.0",
+    description="My personal utilities",
+    author="ziep",
+    license="MIT"
+)
